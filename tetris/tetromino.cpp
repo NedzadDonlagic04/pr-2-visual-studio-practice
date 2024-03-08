6c0a151c4c21e3d4d7ce7b6fb2bf9247cd8ca883
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-03-08 20:42:54 +0100

    Fixed rotate method condition

--- tetris/tetromino.cpp
+++ tetris/tetromino.cpp
@@ -11,11 +11,19 @@ namespace tetromino {
 		: m_shape(shape.begin(), shape.end())
 	{}
 
+	std::size_t Tetromino::getShapeHeight() const noexcept {
+		return m_shape.size();
+	}
+
+	std::size_t Tetromino::getShapeWidth() const noexcept {
+		return m_shape[0].size();
+	}
+
 	void Tetromino::rotate(const bool rotateRight) noexcept {
 		const std::size_t endIndex{ m_shape.size() - 1 };
 
 		for (std::size_t i = 0; i < endIndex - i; i++) {
-			for (std::size_t ii = i; ii < endIndex; ii++) {
+			for (std::size_t ii = i; ii < endIndex - i; ii++) {
 				if (rotateRight) {
 					std::swap(m_shape[i][ii], m_shape[ii][endIndex - i]);
 					std::swap(m_shape[i][ii], m_shape[endIndex - i][endIndex - ii]);
