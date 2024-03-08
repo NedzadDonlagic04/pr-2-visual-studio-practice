32e48f3e7d69860217514ff7b17480288851f5af
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-03-08 15:45:57 +0100

    Moved around terminalBgColors and terminalFgColors

--- tetris/terminalColors.hpp
+++ tetris/terminalColors.hpp
@@ -33,20 +33,6 @@ namespace termColors {
 		grey,
 	};
 
-	// Foreground colors / the color of symbols printed to the terminal
-	const inline std::unordered_map<TerminalFgColors, std::string_view> terminalFgColors{
-		{ TerminalFgColors::lightBlue, "\033[38;5;14m"sv  },
-		{ TerminalFgColors::darkBlue , "\033[38;5;21m"sv  },
-		{ TerminalFgColors::orange   , "\033[38;5;208m"sv },
-		{ TerminalFgColors::yellow   , "\033[38;5;226m"sv },
-		{ TerminalFgColors::green    , "\033[38;5;46m"sv  },
-		{ TerminalFgColors::red      , "\033[38;5;160m"sv },
-		{ TerminalFgColors::magenta  , "\033[38;5;129m"sv },
-		{ TerminalFgColors::black    , "\033[38;5;0m"sv   },
-		{ TerminalFgColors::white    , "\033[38;5;255m"sv },
-		{ TerminalFgColors::grey     , "\033[38;5;249m"sv },
-	};
-
 	// Background colors
 	const inline std::unordered_map<TerminalBgColors, std::string_view> terminalBgColors{
 		{ TerminalBgColors::lightBlue, "\033[48;5;14m"sv  },
@@ -59,6 +45,20 @@ namespace termColors {
 		{ TerminalBgColors::black    , "\033[48;5;0m"sv   },
 		{ TerminalBgColors::white    , "\033[48;5;255m"sv },
 		{ TerminalBgColors::grey     , "\033[48;5;249m"sv },
+	};
+
+	// Foreground colors / the color of symbols printed to the terminal
+	const inline std::unordered_map<TerminalFgColors, std::string_view> terminalFgColors{
+		{ TerminalFgColors::lightBlue, "\033[38;5;14m"sv  },
+		{ TerminalFgColors::darkBlue , "\033[38;5;21m"sv  },
+		{ TerminalFgColors::orange   , "\033[38;5;208m"sv },
+		{ TerminalFgColors::yellow   , "\033[38;5;226m"sv },
+		{ TerminalFgColors::green    , "\033[38;5;46m"sv  },
+		{ TerminalFgColors::red      , "\033[38;5;160m"sv },
+		{ TerminalFgColors::magenta  , "\033[38;5;129m"sv },
+		{ TerminalFgColors::black    , "\033[38;5;0m"sv   },
+		{ TerminalFgColors::white    , "\033[38;5;255m"sv },
+		{ TerminalFgColors::grey     , "\033[38;5;249m"sv },
 	};
 }
 #endif
