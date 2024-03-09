779eb78d9229c5d0ba058f7d8be571adf831ea72
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-03-09 23:34:03 +0100

    Made a lot of changes in hopes of creating a more smooth and logical group of classes to represent the game logic

--- tetris/tetrisGame.cpp
+++ tetris/tetrisGame.cpp
@@ -1,17 +1,125 @@
 #include"tetrisGame.hpp"
 #include"utils.hpp"
 #include<iostream>
+#include<iomanip>
+#include"terminalColors.hpp"
+
+using TerminalBgColor = terminalColors::BackgroundColors;
 
 namespace tetris {
-	TetrisGame::TetrisGame() {
+	// -------------------------------------
+	// LinesClearedInfo class definition
+	// -------------------------------------
+	
+	// public members below
+	LinesClearedInfo::LinesClearedInfo(const std::string_view linesInfoText, const std::size_t m_windowWidth)
+		: m_lineInfoText(linesInfoText)
+		, m_linesCleared(0)
+		, m_windowWidth(m_windowWidth)
+	{}
 
+	std::string_view LinesClearedInfo::getLineInfoText() const noexcept {
+		return m_lineInfoText;
 	}
+
+	int LinesClearedInfo::getLinesCleared() const noexcept {
+		return m_linesCleared;
+	}
+
+	void LinesClearedInfo::incrementLinesCleared() noexcept {
+		++m_linesCleared;
+
+		if (m_linesCleared > 999) {
+			m_linesCleared = 0;
+		}
+	}
+
+	void LinesClearedInfo::printLinesClearedInfo() const noexcept {
+		// Save the initial formatting for std::cout
+		std::ios initialFormatting{ nullptr };
+		initialFormatting.copyfmt(std::cout);
+
+		std::cout << m_lineInfoText << " - ";
+		std::cout << std::setw(3) << std::setfill('0') << m_linesCleared << '\n';
+
+		// Restore initial std::cout formatting
+		std::cout.copyfmt(initialFormatting);
+	}
+
+	// -------------------------------------
+	// PlayField class definition
+	// -------------------------------------
+	
+	// public members below
+	PlayField::PlayField(const std::size_t rowSize, const std::size_t colSize, const std::string_view playFieldBlock)
+		: m_playField(std::vector<std::vector<TerminalBgColor>>(rowSize + spawnAreaHeight, std::vector<TerminalBgColor>(colSize, TerminalBgColor::Default)))
+		, m_playFieldBlock(playFieldBlock)
+	{}
+
+	std::string_view PlayField::getPlayFieldBlock() const noexcept {
+		return m_playFieldBlock;
+	}
+
+	std::size_t PlayField::getPlayFieldWidth() const noexcept {
+		return m_playField[0].size();
+	}
+	
+	std::size_t PlayField::getPlayFieldHeight() const noexcept {
+		return m_playField.size();
+	}
+
+	void PlayField::printPlayField(const bool showSpawnArea) const noexcept {
+		// Used for debugging
+		if (showSpawnArea) {
+			printChunkOfPlayField(0, spawnAreaHeight);
+		}
+
+		printChunkOfPlayField(spawnAreaHeight, getPlayFieldHeight());
+		printPlayFieldBorderLine();
+	}
+
+	// private members below
+
+	void PlayField::printPlayFieldBorderLine() const noexcept {
+		// The + 2 in the condition is for it to take into account the sides
+		// you can remove it to see it's effects better
+		for (std::size_t i = 0; i < m_playField[0].size() + 2; i++) {
+			utils::printTextWithBgColor(getPlayFieldBlock(), borberColor);
+		}
+		std::cout << '\n';
+	}
+
+	void PlayField::printChunkOfPlayField(const std::size_t start, const std::size_t end) const noexcept {
+		printPlayFieldBorderLine();
+
+		for (std::size_t i = start; i < end; i++) {
+			utils::printTextWithBgColor(getPlayFieldBlock(), borberColor);
+
+			for (std::size_t ii = 0; ii < m_playField[i].size(); ii++) {
+				utils::printTextWithBgColor(getPlayFieldBlock(), m_playField[i][ii]);
+			}
+
+			utils::printTextWithBgColor(getPlayFieldBlock(), borberColor);
+			std::cout << '\n';
+		}
+	}
+
+	// -------------------------------------
+	// TetrisGame class definition
+	// -------------------------------------
+	
+	// public members below
+	TetrisGame::TetrisGame() 
+		: PlayField(intendedGameScreenHeight, intendedGameScreenWidth)
+		, LinesClearedInfo("Lines cleared", intendedGameScreenWidth)
+	{}
 
 	void TetrisGame::run() {
 		printGameIntroInfo();
 		startMainGameLoop();
 	}
 
+	// private members below
 	void TetrisGame::printGameIntroInfo() const noexcept {
 		std::cout << "Hello and welcome to my attempt at recreating Tetris in the terminal\n";
 		std::cout << "Preferably resize the terminal to take up the full screen so nothing goes out of bounds\n\n";
@@ -27,6 +135,13 @@ namespace tetris {
 	}
 
 	void TetrisGame::startMainGameLoop() {
-		utils::delayMs(2000);
+		printLinesClearedInfo();
+
+		// put true here to show the spawn area for the
+		// tetrominos
+		printPlayField(true);
+
+		// utils::delayMs(2000);
+		// utils::clearScreen();
 	}
 }
--- tetris/tetrisGame.hpp
+++ tetris/tetrisGame.hpp
@@ -1,8 +1,61 @@
 #ifndef _TETRIS_TETRIS_GAME_HPP
 #define _TETRIS_TETRIS_GAME_HPP
 
+#include<vector>
+#include"terminalColors.hpp"
+
+using TerminalBgColor = terminalColors::BackgroundColors;
+
 namespace tetris {
-	class TetrisGame {
+	// -------------------------------------
+	// LinesClearedInfo class declaration
+	// -------------------------------------
+	class LinesClearedInfo {
+	public:
+		LinesClearedInfo() = default;
+		LinesClearedInfo(const std::string_view, const std::size_t = 0);
+
+		[[nodiscard]] std::string_view getLineInfoText() const noexcept;
+		[[nodiscard]] int getLinesCleared() const noexcept;
+
+		void incrementLinesCleared() noexcept;
+		void printLinesClearedInfo() const noexcept;
+	private:
+		std::string_view m_lineInfoText{};
+		int m_linesCleared{};
+		const std::size_t m_windowWidth{};
+	};
+
+	// -------------------------------------
+	// PlayField class declaration
+	// -------------------------------------
+	class PlayField {
+	public:
+		PlayField() = default;
+		PlayField(const std::size_t, const std::size_t, const std::string_view = "  ");
+		
+		[[nodiscard]] std::string_view getPlayFieldBlock() const noexcept;
+		[[nodiscard]] std::size_t getPlayFieldWidth() const noexcept;
+		[[nodiscard]] std::size_t getPlayFieldHeight() const noexcept;
+
+		void printPlayField(const bool = false) const noexcept;
+	private:
+		const std::vector<std::vector<TerminalBgColor>> m_playField{};
+		const std::string_view m_playFieldBlock {};
+
+		constexpr static std::size_t spawnAreaHeight{ 5 };
+		constexpr static TerminalBgColor borberColor{ TerminalBgColor::grey };
+
+		void printPlayFieldBorderLine() const noexcept;
+		void printChunkOfPlayField(const std::size_t, const std::size_t) const noexcept;
+	};
+
+	// -------------------------------------
+	// TetrisGame class declaration
+	// -------------------------------------
+	class TetrisGame 
+		: public PlayField
+		, public LinesClearedInfo {
 	public:
 		TetrisGame();
 
@@ -10,6 +63,9 @@ namespace tetris {
 	private:
 		void printGameIntroInfo() const noexcept;
 		void startMainGameLoop();
+
+		constexpr static std::size_t intendedGameScreenWidth{ 10 };
+		constexpr static std::size_t intendedGameScreenHeight { 20 };
 	};
 }
 
